d05f064480a4a64ba7d6361d4976d9c24929e43b
Jonathan Rico <jonathan.rm@gmail.com>
2014-10-21 15:14:05 -0700

    removed custom components folder

--- custom/build/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TARGET_TO_RUN="$1"
-#JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Versions/1.5.0/Home"
-#ANT_HOME=${BUILD_TOOLS_HOME}"/ant/apache-ant-1.7.1"
-#PATH=$JAVA_HOME/bin:$ANT_HOME/bin:$PATH
-
-echo  "Executing : ${TARGET_TO_RUN}"
-ant ${TARGET_TO_RUN} -DcheckOnly=false -DrunAllTests=true -f build.xml

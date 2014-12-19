3bae001b0b1f24d85223aa0014fe469281848bdd
Luis Campos <lcamposguajardo@gmail.com>
2014-12-19 04:46:54 -0800

    adding Grunt for static resource processing

---
+++ build/Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+  
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    copy: {
+      files: {
+        cwd: '../static-resources/almond_uilib',
+        src: '**/*',
+        dest: './dist',
+        expand: true
+      }
+    },
+    uglify: {
+      build: {
+        files: {
+          './dist/js/almondutils.js': ['./dist/js/almondutils.js'],
+          './dist/js/fastclick.js': ['./dist/js/fastclick.js'],
+          './dist/js/scrollfix.js': ['./dist/js/scrollfix.js']
+        }
+      }
+    },
+    cssmin: {
+      build: {
+        expand: true,
+        cwd: './dist/css/',
+        src: ['*.css', '!*.min.css'],
+        dest: './dist/css/',
+        ext: '.css'
+      }
+    },
+    compress: {
+      almond: {
+        options: {
+          archive: '../app/src/staticresources/almond_uilib.resource',
+          mode: 'zip'
+        },
+        files: [
+          { src: './dist/**' }
+          ]
+      }
+    },
+    clean : {
+      almondCleanup : {
+        src : [ './dist/']
+      }
+    },
+    watch: {
+      js: {
+        files: ['../static-resources/almond_uilib/js/*.js'],
+        tasks: ['uglify']
+      },
+      css: {
+        files: ['../static-resources/almond_uilib/css/*.css'],
+        tasks: ['cssmin']
+      }
+    }
+
+  });
+  
+  // Default task(s).
+  grunt.registerTask('default', ['copy', 'uglify', 'cssmin', 'compress', 'clean']);
+
+};
